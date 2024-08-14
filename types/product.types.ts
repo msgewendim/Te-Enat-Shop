4b051fdf63ab734a03d80e33bc5a007fa017c106
misganaw <msganaw12113@gmail.com>
2024-08-14 17:02:07 +0300

    shopPage: getting products from api, context proviedr for all App

--- types/product.types.ts
+++ types/product.types.ts
@@ -2,31 +2,21 @@ import { Review } from "./recipe.types";
 interface Product {
   _id: string;
   name: string;
-  description: string;
+  shortDescription: string;
   price: number;
   images: string[];
   rate: number;
-  category: string;
-  stock: number;
+  categories: string[];
+  InStock: number;
   availability: "In Stock" | "Out of Stock" | "Pre-order";
-  weight: string;
-  ingredients?: string[];
-  nutritionalInfo?: NutritionalInfo;
-  allergens?: string[];
-  originalPrice?: number;
-  discountedPrice?: number;
-  discountPercentage?: number;
-  reviews: Review[];
-  relatedProducts: Partial<Product>[];
+  weights: string[];
+  benefits?: Array<String>;
+  reviews?: Review[];
+  relatedProducts?: Partial<Product>[];
   totalSales?: number;
 }
 
-interface NutritionalInfo {
-  calories: number;
-  fat: string;
-  protein: string;
-  carbohydrates: string;
-}
+
 
 interface Languages {
   en: {
