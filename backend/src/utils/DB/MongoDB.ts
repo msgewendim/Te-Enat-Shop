3629fdae8c3ced26033d09860ab5e77e6582943b
misganaw <msganaw12113@gmail.com>
2024-10-25 18:04:51 +0300

    deploy-log

--- backend/src/utils/DB/MongoDB.ts
+++ backend/src/utils/DB/MongoDB.ts
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
-import products from "../../../products.json";
-import ProductModel from "../../models/ProductSchema";
+
 const connectToMongoDB = async (mongoUri: string) => {
   try {
     // connect to mongoDB
     await mongoose.connect(mongoUri);
     console.log("connected to the mongodb");
-    // await ProductModel.insertMany(products);
-    // console.log("Products added successfully");
   } catch (error) {
     console.log(error);
   }
