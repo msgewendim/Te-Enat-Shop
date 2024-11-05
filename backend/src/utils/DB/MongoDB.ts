333fbba8ca5d11c86076ebd4f894936502cd6552
misganaw <msganaw12113@gmail.com>
2024-11-05 17:43:55 +0200

    chore:fix type for depoly api

--- backend/src/utils/DB/MongoDB.ts
+++ backend/src/utils/DB/MongoDB.ts
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
-import recipes from "../../../recipes.json";
-import RecipeModel from "../../models/RecipeSchema";
 
 const connectToMongoDB = async (mongoUri: string) => {
   try {
     // connect to mongoDB
     await mongoose.connect(mongoUri);
     console.log("connected to the mongodb");
-    // const Recipes = await RecipeModel.insertMany(recipes);
-    // console.log("Recipes created successfully");
   } catch (error) {
     console.log(error);
   }
